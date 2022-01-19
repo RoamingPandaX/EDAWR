d7f34749fa195fdfb23bdb4ffafe9fbe0f8124c0
RoamingPandaX <lenguyenquochung2013346032@ftu.edu.vn>
2022-01-19 16:05:23 +0700

    Quick update
    my learning progress

--- R/my_note.R
+++ R/my_note.R
@@ -2,5 +2,25 @@ install.packages("devtools")
 devtools::install_github("rstudio/EDAWR")
 
 library(tidyr)
+library(EDAWR)
 
+# basic function in the package 
+?gather
+?spread
+?unite
+?separate
+?select
 
+# Looking at the data
+storms
+cases
+pollution
+
+# Tidying data
+gather(cases, "year", "n", 2:4)
+spread(pollution, size,  "amount")
+
+storms2 <- separate(storms, date, c("Year","Month","Day"), sep = "-")
+unite(storms2, "date",Year,Month,Day,sep="-" )
+
+# 
